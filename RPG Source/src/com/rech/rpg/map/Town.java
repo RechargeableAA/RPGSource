341449fd4d6bf8b7f3f5ba6ab8a89533f9b9fc04
RechargeableAA <thelivingcrittles@gmail.com>
2022-02-27 19:08:04 -0600

    Town name generator
    is based off 2 arrays of preset names

--- RPG Source/src/com/rech/rpg/map/Town.java
+++ RPG Source/src/com/rech/rpg/map/Town.java
@@ -1,6 +1,7 @@
 package com.rech.rpg.map;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import com.rech.rpg.item.Potion;
 import com.rech.rpg.map.shop.PotionShop;
@@ -20,6 +21,27 @@ public class Town extends Location{
 		
 		shops[0] = new PotionShop("Alman's Potions", new Potion[] {Potion.minorHealth, Potion.standardHealth, Potion.minorMana});
 	}
+	
+	
+	public static String generateTown(int firstName, int lastName) {
+		String[] stringA = new String[] {"Gray", "Green", "Kil", "Bleak", "Dusk", "Storm", "Summer", "Never", "Dire", "North", "East", "South", "West"}; //13 total
+		String[] stringB = new String[] {"drift", "ville", "rock", "stone", "host", "wood", "valley", "felt", "shore", "beach", "port", "watch"}; //12 total
+		Random rand = new Random(); //can change to 13, figured length of A would always be larger than B
+		String townName;
+		
+		try {
+			if (firstName == 0 && lastName == 0) { //0,0 will ask to make a random town. any other number will pick a specific name based on the arrays
+				townName = stringA[rand.nextInt(stringA.length)]+stringB[rand.nextInt(stringB.length)];
+			}else {
+				townName = stringA[firstName]+stringB[lastName];//placeholder for final name
+			}
+			return townName;
+		}catch (ArrayIndexOutOfBoundsException IOB){
+			IOB.printStackTrace();
+			return "ERROR"; //can set to GrayDrift if preferred.
+		}
+	}
+	
 	
 	@Override
 	public String getSurroundings() {
